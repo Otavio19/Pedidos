f8927ab5b032b84ff6a113cacdde6449c0ca548a
Otavio19 <otavioafonso@gmail.com>
2022-11-22 00:00:20 -0300

    Login Implemented

--- src/app/services/product-service.service.ts
+++ src/app/services/product-service.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 
 import { HttpInterceptor, HttpRequest, HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, Observable } from 'rxjs';
+import { parse } from '@fortawesome/fontawesome-svg-core';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,11 @@ export class ProductServiceService {
   //private urlApi = 'https://62d87c1890883139359173dd.mockapi.io/produto';
   private urlApi= 'https://x8ki-letl-twmt.n7.xano.io/api:Az8xGr_h/Produtos';
   private token = localStorage.getItem('token')
-  private head_obj = new HttpHeaders().set('Authorization','bearer '+ this.token)
+  private tokenString = String(this.token)
+  private tokenCerto = this.tokenString.split('"')
+  private head_obj = new HttpHeaders().set('Authorization','bearer '+ this.tokenCerto[3])
 
-  constructor( private http : HttpClient) {
-    console.log(this.token)
-  }
+  constructor( private http : HttpClient) {}
 
   getAll():Observable<IProduto[]>{
     return this.http.get<IProduto[]>(this.urlApi, {headers:(this.head_obj)});
